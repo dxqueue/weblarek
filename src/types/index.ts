8dcdba9bda3b265f51eda1ccdf87d3eca3d61f0d
Alexandr Nazarov <stig8x1@gmail.com>
2025-09-02 20:40:48 +0300

    first

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,3 +4,24 @@ export interface IApi {
     get<T extends object>(uri: string): Promise<T>;
     post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
 }
+
+export type ICategory = 'другое' | 'софт-скил' | 'хард-скил' | 'кнопка' | 'дополнительное';
+
+export interface IProduct {
+    id: string;
+    title: string;
+    image: string;
+    category: ICategory;
+    price: number | null;
+    description: string;
+}
+
+export type IPayment = 'card' | 'cash' | '';
+
+
+ export interface IBuyer {
+    payment: IPayment;
+    address: string;
+    email: string;
+    phone: string;
+ }
